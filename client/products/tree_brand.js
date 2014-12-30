0814eec2fa50588723c5e4b986b352190e2e885f
Fredi Mattwich <frederik@mattwich.de>
2014-12-30 14:53:35 +0100

    Neue Collections: Brands für alle Marken, Categories für alle Kategorien.. Man sollte sich noch was überlegen um das dynamisch zu machen

--- client/products/tree_brand.js
+++ client/products/tree_brand.js
@@ -1,6 +1,9 @@
 Template.brandTree.helpers({
     tree: function () {
         var products = Products.find().fetch();
+    },
+    categories: function(){
+    	return Categories.find();
     }
 });
 
--- server/start.js
+++ server/start.js
@@ -1,32 +1,61 @@
 Meteor.startup(function () {
-    Products.insert({
-        title: "Schuh1",
-        brand: "Gabor",
-        category: "Stiefel"
-    });
-    Products.insert({
-        title: "Schuh2",
-        brand: "Gabor",
-        category: "Stiefel"
-    });
-    Products.insert({
-        title: "Schuh3",
-        brand: "Gabor",
-        category: "Stiefel"
-    });
-    Products.insert({
-        title: "Schuh4",
-        brand: "Gabor",
-        category: "Stiefel"
-    });
-    Products.insert({
-        title: "Schuh1Laufen",
-        brand: "Adidas",
-        category: "Laufschuh"
-    });
-    Products.insert({
-        title: "Schuh2Laufen",
-        brand: "Adidas",
-        category: "Laufschuh"
-    });
+    if(Categories.find().count()==0){
+        Categories.insert({
+            title: "Stiefel"
+        });
+        Categories.insert({
+            title: "Laufschuh"
+        });
+        Brands.insert({
+            title: "Gabor"
+        });
+        Brands.insert({
+            title: "Adidas"
+        });
+        Products.insert({
+            title: "Schuh1",
+            brand: getBrandID("Gabor"),
+            category: getCategoryID("Stiefel"),
+        });
+        Products.insert({
+            title: "Schuh2",
+            brand: getBrandID("Gabor"),
+            category: getCategoryID("Stiefel"),
+        });
+        Products.insert({
+            title: "Schuh3",
+            brand: getBrandID("Adidas"),
+            category: getCategoryID("Stiefel"),
+        });       
+        Products.insert({
+            title: "Schuh4",
+            brand: getBrandID("Adidas"),
+            category: getCategoryID("Stiefel"),
+        });        
+        Products.insert({
+            title: "Schuh5",
+            brand: getBrandID("Gabor"),
+            category: getCategoryID("Laufschuh"),
+        });        
+        Products.insert({
+            title: "Schuh6",
+            brand: getBrandID("Gabor"),
+            category: getCategoryID("Laufschuh"),
+        });        
+        Products.insert({
+            title: "Schuh7",
+            brand: getBrandID("Aduadas"),
+            category: getCategoryID("Laufschuh"),
+        });
+
+    }
 });
+
+getBrandID = function(name){
+    var obj = Brands.findOne({title: name});
+    return obj._id;
+}
+getCategoryID = function(name){
+    var obj = Categories.findOne({title: name});
+    return obj._id;
+}
